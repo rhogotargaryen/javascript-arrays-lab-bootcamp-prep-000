09659bc102228cb0ad4f8010e69343eef88a6e99
Derek Haller <dhaller.personal@gmail.com>
2017-09-15 22:36:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 var kittens = ['Garfield', 'Tom', 'Milo']
 function destructivelyAppendKitten(name) {
-  kittens.unshift(name);
+  kittens.shift(name);
   return kittens;
 }
