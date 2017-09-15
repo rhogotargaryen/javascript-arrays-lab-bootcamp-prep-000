f77c461bda34ebeeadc87b0f0c22566843152185
Derek Haller <dhaller.personal@gmail.com>
2017-09-15 23:05:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,3 +35,8 @@ function removeLastKitten() {
   var newKittenArray = kittens.slice(0, kittens.length - 1);
   return newKittenArray;
 }
+
+function removeFirstKitten() {
+  var newKittenArray = kittens.slice(1);
+  return newKittenArray;
+}
