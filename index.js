4a9d61940a03f791a5141ebb114aff9d077dfb07
Derek Haller <dhaller.personal@gmail.com>
2017-09-15 22:45:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function destructivelyAppendKitten(name) {
   return kittens;
 }
 
-function desturctivelyPrependKitten(name) {
+function destructivelyPrependKitten(name) {
   kittens.unshift(name);
   return kittens;
 }
